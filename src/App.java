fc9649564ead7def58d6c0e439f05b446b1c3b64
Lotta <amk1022106@student.hamk.fi>
2026-09-22 16:07:01 +0300

    Korjattujen lukujen arvot

--- src/App.java
+++ src/App.java
@@ -4,8 +4,8 @@ public class App {
         System.out.println("Ohjelman tekija: ");
         String tekija = "Lotta";
         System.out.println(tekija);
-        double luku1 = 3;
-        double luku2 = 6;
+        double luku1 = 5;
+        double luku2 = 2;
         System.out.println(luku1);
         System.out.println(luku2);
         System.out.println("Luku1-muuttujan arvo on: " + luku1);
