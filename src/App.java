9103a0d30289af5c2df73b81ac7c1201a5eb8223
Lotta <amk1022106@student.hamk.fi>
2026-09-22 15:32:32 +0300

    Lisätty tulostinohjelman perusversio

---
+++ src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hei olen Tulostin-ohjelma");
+        System.out.println("Ohjelman tekija: ");
+        String tekija = "Lotta";
+        System.out.println(tekija);
+        double luku1 = 7;
+        double luku2 = 9;
+        System.out.println(luku1);
+        System.out.println(luku2);
+        System.out.println("Luku1-muuttujan arvo on: " + luku1);
+        System.out.println("Luku2-muuttujan arvo on: " + luku2);
+        final double tulo = luku1 * luku2;
+        System.out.println("Tulo on: " + tulo);
+
+}
